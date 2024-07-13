957c2238c5919926d1a4da644a41137be9d5042a
hehehe84 <antoine.picott@gmail.com>
2024-07-13 14:46:56 +0200

    delete app change app1 to app

--- app2/src/app/utils/constants.ts
+++
@@ -1,5 +0,0 @@
-import { PublicKey } from "@solana/web3.js";
-
-export const PROGRAM_ID = new PublicKey(
-  "HqDyiG5gywghGwTAg3dAxMtAAdUoQmqkCPf6zK34vumS" // UPDATE 1 - Enter the publicKey of your program
-);
