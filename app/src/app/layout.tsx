9885563a3cb153579d1f81c10322aae2f1f48e10
hehehe84 <antoine.picott@gmail.com>
2024-07-20 10:57:39 +0200

    remise en ordre header + Petit responsive

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react';
 import { AppProvider } from './contexts/AppProvider';
+import { ChakraProvider } from '@chakra-ui/react';
 import dynamic from 'next/dynamic';
 import Header from './components/Header';
 import './globals.css';
@@ -13,14 +14,16 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <ClientWalletProviderComponent>
-          <AppProvider>
-            <Header />
-            <main className="container mx-auto px-4 py-8">
-              {children}
-            </main>
-          </AppProvider>
-        </ClientWalletProviderComponent>
+        <ChakraProvider>
+          <ClientWalletProviderComponent>
+            <AppProvider>
+              <Header />
+              <main className="container mx-auto px-4 py-8">
+                {children}
+              </main>
+            </AppProvider>
+          </ClientWalletProviderComponent>
+        </ChakraProvider>
       </body>
     </html>
   );
