ca7cf59faf40dec156796340d03e43bb2531f847
hehehe84 <antoine.picott@gmail.com>
2024-07-20 17:37:10 +0200

    one Instance to access BDD to write/call

--- app/src/app/api/pixels-color-update/route.ts
+++ app/src/app/api/pixels-color-update/route.ts
@@ -1,12 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { PixelService } from '../../services/PixelService';
-import { RedisPixelRepository } from '../../repositories/RedisPixelRepository';
-
-const pixelService = new PixelService(new RedisPixelRepository());
+import SingletonPixelService from '../pixelService';
 
 export async function POST(request: NextRequest) {
   try {
     const { pixels, owner } = await request.json();
+    const pixelService = SingletonPixelService.getInstance().getPixelService();
     await pixelService.buyPixels(pixels, owner);
     return NextResponse.json({ message: 'Pixels bought successfully' });
   } catch (error) {
