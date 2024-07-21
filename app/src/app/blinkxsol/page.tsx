e44fdc6db903aea223eae8c9a3b90d92d005cdb5
ToLan <t.landes84@gmail.com>
2024-07-21 23:18:49 +0200

    final push

--- app/src/app/blinkxsol/page.tsx
+++ app/src/app/blinkxsol/page.tsx
@@ -1,10 +1,12 @@
+"use client";
+
 import Link from 'next/link';
 
 const Page = () => {
   return (
     <div>
-      <Link href="https://dial.to/?action=solana-action:http://localhost:3000/api/blink">       
-          <button>Go to Link</button>        
+      <Link href="https://dial.to/?action=solana-action:http://localhost:3000/api/blink">
+        <button>Go to Link</button>
       </Link>
     </div>
   );
